555a381e4ba703bd4f9abd41dd5f5604e1162efa
Damian Spizzirri <damianspizzirri@gmail.com>
2021-04-29 23:47:21 -0300

    septimo ejemplo - ejemplo attributeChangedCallback

---
+++ attributeChangedCallback/my-element.js
@@ -0,0 +1,58 @@
+class myElement extends HTMLElement {
+
+    constructor(){
+        super();
+        this.attachShadow({
+            mode: 'open'
+        });
+    }
+
+    static get observedAttributes(){
+        return ['title', 'parrafo', 'img']
+    }
+
+    attributeChangedCallback(atributo, valorViejo, valorNuevo){
+        if(atributo === 'title'){
+            this.title = valorNuevo;
+        }
+        if(atributo === 'parrafo'){
+            this.parrafo = valorNuevo;
+        }
+        if(atributo === 'img'){
+            this.img = valorNuevo;
+        }
+    }
+
+    getTemplate(){
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <section>
+                <h2>${this.title}</h2>
+                <div>
+                    <p>${this.parrafo}</p>
+                    <img src=${this.img}/>
+                </div>
+            </section>
+            ${this.getStyles()}
+        `;
+        return template;
+    }
+
+    getStyles(){
+        return `  
+        <style>
+            h2 { color: red; }
+        </style>
+        `
+    }
+
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+}
+
+customElements.define('my-element', myElement);
